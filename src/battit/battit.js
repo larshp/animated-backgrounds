9c402095e0bbef502472839302bb50523f5dbd92
larshp <larshp@hotmail.com>
2017-03-04 18:27:41 +0000

    hmm

--- src/battit/battit.js
+++ src/battit/battit.js
@@ -2,13 +2,35 @@ let points = [];
 let ctx = null;
 var timer = null;
 
+function distance(a, b) {
+  return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+}
+
+function findClosest(point) {
+  let closest = [];
+
+  for(let i=0;i<points.length;i++) {
+    if(point.x === points[i].x && point.y === points[i].y) {
+      continue;
+    }
+    closest.push({distance: distance(point, points[i]), from: point, to: points[i]});
+  }
+
+  closest = closest.sort(function(a, b) {
+    return a.distance - b.distance;
+  }).slice(0, 3);
+
+  return closest;
+}
+
 function draw() {
-  console.log("draw");
   ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+  let closest = [];
 
   for(let i=0;i<points.length;i++) {
     ctx.beginPath();
-    ctx.arc(points[i].x, points[i].y, 2, 0, Math.PI*2, true);
+    ctx.arc(points[i].x, points[i].y, 3, 0, Math.PI*2, true);
     ctx.closePath();
     ctx.fillStyle = '#00ff00';
     ctx.fill();
@@ -16,13 +38,25 @@ function draw() {
     points[i].x = points[i].x + points[i].vector.x;
     points[i].y = points[i].y + points[i].vector.y;
 
+    closest = closest.concat(findClosest(points[i]));
+
     if (points[i].x > window.innerWidth || points[i].x < 0) {
       points[i].vector.x = -points[i].vector.x;
     }
     if (points[i].y > window.innerHeight || points[i].y < 0) {
       points[i].vector.y = -points[i].vector.y;
     }
+  }
 
+// todo, remove identical from closest(from + to)
+
+  for(let i=0;i<closest.length;i++) {
+    ctx.beginPath();
+    ctx.moveTo(closest[i].from.x,closest[i].from.y);
+    ctx.lineTo(closest[i].to.x,closest[i].to.y);
+    ctx.strokeStyle = '#ff0000';
+    ctx.stroke();
+    ctx.closePath();
   }
 
   timer = setTimeout(draw, 50);
@@ -36,15 +70,16 @@ function initialize(count = 30) {
     ctx = canvas.getContext('2d');
 
     for(let i=0;i<count;i++) {
+      let vector = {
+          x: Math.random() * (Math.random() < 0.5 ? 1 : -1),
+          y: Math.random() * (Math.random() < 0.5 ? 1 : -1)};
+
       points.push({
         x: Math.floor(Math.random() * window.innerWidth) + 1 ,
         y: Math.floor(Math.random() * window.innerHeight) + 1,
-        vector: {
-          x: Math.random() * Math.random() < 0.5 ? 1 : -1,
-          y: Math.random() * Math.random() < 0.5 ? 1 : -1}
-        });
+        vector: vector});
     }
-    console.dir(points);
+//    console.dir(points);
 
     draw();
   }
